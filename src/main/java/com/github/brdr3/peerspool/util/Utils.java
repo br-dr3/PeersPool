d49fed6fc5fe26de1e6a848095b5684dc750f0d6
br-dr3 <brian_aandreossi@hotmail.com>
2019-08-03 15:56:43 -0300

    First branch commit

--- src/main/java/com/github/brdr3/peerspool/util/Utils.java
+++
@@ -1,9 +0,0 @@
-package com.github.brdr3.peerspool.util;
-public class Utils {
-    
-    public static void cleanBuffer(byte[] buffer) {
-        for (int i = 0; i < buffer.length; i++) {
-            buffer[i] = 0;
-        }
-    }
-}
